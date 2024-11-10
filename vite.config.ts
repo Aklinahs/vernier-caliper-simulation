4ebdcfa047e31300f75472185448cbd9121a6dfe
Aklinahs <shanilka.Ariyarathne@Gmail.Com>
2024-11-10 20:52:04 +0530

    updated nessasary details for GitHUb pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/vernier-caliper-simulation/', // Add this line - should match your repository name
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
